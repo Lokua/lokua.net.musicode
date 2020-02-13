c01c716d1b48fe79e78184d720ec49656a378e23
Joshua Kleckner <lokua@lokua.net>
2020-02-13 00:19:20 -0600

    chore: cleanup

--- src/index.spec.mjs
+++
@@ -1,7 +0,0 @@
-// import test from './test-framework.mjs'
-// import assert from 'assert'
-// import { getSongPositionTicks } from './index.mjs'
-
-// test('getSongPositionTicks', () => {
-//   assert.deepEqual(getSongPositionTicks([, 0, 8]), 0)
-// })
